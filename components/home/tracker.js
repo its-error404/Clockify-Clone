fac7e69c6b07455dd0c99d9caa568eb4fa5df6cb
karthikeyan_AK <karthikeyan.athipalam@rootquotient.com>
2023-08-21 17:09:49 +0530

    Implementing duplicate project

--- components/home/tracker.js
+++ components/home/tracker.js
@@ -3,28 +3,12 @@ document.addEventListener("DOMContentLoaded", () => {
   const timerDisplay = document.getElementById("timer");
   const projectDescriptionInput = document.getElementById("project-description");
   const manualStartDateInput = document.getElementById("manual-start-date");
+  const manualStartDateInputInsider = document.getElementById('manual-start-date-insider')
   const timeInfoContainer = document.getElementById("time-info");
-  const weekHeader = document.getElementById("week-date-range");
-  const calculateWeekTime = document.querySelector(".calculated-week-time");
 
   let timerInterval;
   let startTime;
   let takenTime = 0;
-
-  startButton.addEventListener("click", () => {
-    if (startButton.textContent === "START") {
-      startButton.textContent = "STOP";
-      startButton.style.backgroundColor = "red";
-      startButton.style.color = "white";
-      startTimer();
-    } else {
-      startButton.textContent = "START";
-      startButton.style.backgroundColor = "";
-      startButton.style.color = "";
-      stopTimer();
-      displayWeek();
-    }
-  });
 
   const startTimer = () => {
     takenTime = 0;
@@ -43,7 +27,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const currentTime = Date.now();
     const timeDifference = currentTime - startTime;
     const formattedTime = formatTime(timeDifference);
-    timerDisplay.textContent = formattedTime;
+    timerDisplay.innerHTML = formattedTime;
   };
 
   function formatTime(milliseconds) {
@@ -54,35 +38,30 @@ document.addEventListener("DOMContentLoaded", () => {
     return `${hours}:${minutes}:${seconds}`;
   }
 
-  const dateFormat = (date) => {
-    const options = { day: "2-digit", month: "2-digit", year: "numeric" };
-    return date.toLocaleDateString("en-US", options);
-  };
-
-  const displayWeek = () => {
-    const weekHeader = document.getElementById("week-date-range");
-    const weekStartDate = new Date(manualStartDateInput.value);
-    const weekEndDate = new Date(weekStartDate);
-    weekEndDate.setDate(weekStartDate.getDate() + 6);
-
-    const formattedWeekStartDate = dateFormat(weekStartDate);
-    const formattedWeekEndDate = dateFormat(weekEndDate);
-
-    weekHeader.textContent = `${formattedWeekStartDate} - ${formattedWeekEndDate}`;
-  };
-
   const displayTimeInfo = () => {
-    const trackedTime = timerDisplay.textContent;
+    const trackedTime = timerDisplay.innerHTML;
     const projectDescription = projectDescriptionInput.value;
     const startDate = manualStartDateInput.value;
+
+    const startDateObject = new Date(startDate)
+    const startOfWeek = new Date(startDateObject)
+    const endOfWeek = new Date(startDateObject)
+    
+    startOfWeek.setDate(startDateObject.getDate() - startDateObject.getDay()); 
+    endOfWeek.setDate(startDateObject.getDate() + (6 - startDateObject.getDay()));
+  
+    const weekStartDateString = startOfWeek.toUTCString().substring(0,11);
+    const weekEndDateString = endOfWeek.toUTCString().substring(0,11);
 
     const newTimeEntry = document.createElement("div");
     newTimeEntry.classList.add("time-entry");
     newTimeEntry.innerHTML = `
-      <div class='week-header'>
-          <p id='week-date-range'></p>
-          <p class='week-total'>Week total: <span class=calculated-week-time>${trackedTime}</span></p>
-      </div>
+
+  
+    <div class='week-header'>
+      <p id='week-date-range'>${weekStartDateString} - ${weekEndDateString}</p>
+      <p class='week-total'>Week total: <span class=calculated-week-time>${trackedTime}</span></p>
+    </div>
 
       <div class="tracked-time">
           <div class="date-container">
@@ -94,13 +73,17 @@ document.addEventListener("DOMContentLoaded", () => {
           </div>
           <div class='project-details'>
               <div class='project-info'>
+                  <p>.</p>
                   <p>${projectDescription}</p>
               </div>
               <div class='project-details__other-features'>
-                  <p><strong>Start Date:</strong>&nbsp;${startDate}</p>
+                <div class='tags'>
                   <img src='/assets/View tags.svg' width='20px'>
+                </div>
+                <div class='currency'>
                   <h3>$</h3>
-                  <input type="date" id="manual-start-date">
+                </div>
+                  <input type="date" id="manual-start-date-insider">
                   <p><strong>Time:</strong> ${trackedTime}</p>
                   <img src='/assets/Start button.svg' width='20px' id='continue-button'>
                   <img src='/assets/Edit menu dark theme.svg' width='5px' class='edit-options'>
@@ -118,35 +101,6 @@ document.addEventListener("DOMContentLoaded", () => {
     `;
 
     timeInfoContainer.appendChild(newTimeEntry);
-
-    const projectEntries = document.querySelectorAll(`.project-${projectDescription}`);
-
-    if (projectEntries.length > 1) {
-      const projectInfo = newTimeEntry.querySelector(".project-info");
-      const toggleButton = document.createElement("button");
-      toggleButton.textContent = `Show all ${projectEntries.length} projects`;
-      toggleButton.classList.add("toggle-button");
-      projectInfo.appendChild(toggleButton);
-
-      toggleButton.addEventListener("click", () => {
-        projectEntries.forEach((entry) => {
-          if (entry != newTimeEntry) {
-            entry.classList.toggle("project-hidden");
-          }
-        });
-      });
-    }
-
-    const totalTrackedTime = calculateTotalTime(projectDescription);
-    projectEntries.forEach((entry) => {
-      const totalTrackedTimeElement = entry.querySelector(".calculated-time");
-      totalTrackedTimeElement.textContent = formatTime(totalTrackedTime);
-    });
-
-
-    const totalWeekTime = calculateTotalWeekTime();
-    calculateWeekTime.textContent = formatTime(totalWeekTime);
-
     const editOptions = newTimeEntry.querySelector(".edit-options");
     const editDropdown = newTimeEntry.querySelector(".edit-dropdown");
 
@@ -161,64 +115,25 @@ document.addEventListener("DOMContentLoaded", () => {
       }
     });
 
-    const dateElement = document.getElementById("date");
-    const today = new Date();
-    const formattedDate = dateFormat(today);
-    const todayString = dateFormat(new Date());
+    const duplicateButton = newTimeEntry.querySelector('.duplicate-item')
+    // duplicateButton.addEventListener(('click'), DuplicateTimeInfo)
+  };
 
-    if (formattedDate === todayString) {
-      dateElement.textContent = "Today";
+  const resetTimer = () => {
+    timerDisplay.innerHTML = "00:00:00";
+  };
+
+  startButton.addEventListener("click", () => {
+    if (startButton.innerHTML === "START") {
+      startButton.innerHTML = "STOP";
+      startButton.style.backgroundColor = "red";
+      startButton.style.color = "white";
+      startTimer();
     } else {
-      dateElement.textContent = formattedDate;
+      startButton.innerHTML = "START";
+      startButton.style.backgroundColor = "";
+      startButton.style.color = "";
+      stopTimer();
     }
-  };
-
-  const findSameProjects = (projectDescription) => {
-    const sameEntries = document.querySelectorAll(
-      `.project-${sanitizeClassName(projectDescription)}`
-    );
-    if (sameEntries.length > 0) {
-      return sameEntries[0];
-    } else {
-      return null;
-    }
-  };
-
-  const calculateTotalTime = (projectDescription) => {
-    const entriesWithSameProject = document.querySelectorAll(`.project-${projectDescription}`);
-    let totalMilliseconds = 0;
-
-    entriesWithSameProject.forEach((entry) => {
-      const trackedTimeElement = entry.querySelector(".calculated-time");
-      const trackedTimeParts = trackedTimeElement.textContent.split(":");
-      const hours = parseInt(trackedTimeParts[0]);
-      const minutes = parseInt(trackedTimeParts[1]);
-      const seconds = parseInt(trackedTimeParts[2]);
-      totalMilliseconds = totalMilliseconds + (hours * 3600 + minutes * 60 + seconds) * 1000;
-    });
-    return totalMilliseconds;
-  };
-
-  const calculateTotalWeekTime = () => {
-    const allTrackedTimes = document.querySelectorAll(".time-entry");
-    let totalMilliseconds = 0;
-
-    allTrackedTimes.forEach((time) => {
-      const trackedTimeElement = time.querySelector(".calculated-time");
-      const trackedTimeParts = trackedTimeElement.textContent.split(":");
-      const hours = parseInt(trackedTimeParts[0]);
-      const minutes = parseInt(trackedTimeParts[1]);
-      const seconds = parseInt(trackedTimeParts[2]);
-
-      totalMilliseconds = totalMilliseconds + (hours * 3600 + minutes * 60 + seconds) * 1000;
-    });
-
-    return totalMilliseconds;
-  };
-
-  function resetTimer() {
-    timerDisplay.textContent = "00:00:00";
-  }
+  });
 });
-
-  
